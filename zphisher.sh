4275f9c642483039ef56a02dd4db71412248e869
Mrpratheep <166207213+Mrpratheep@users.noreply.github.com>
2024-08-21 21:17:16 +0530

    Update zphisher.sh
    Name mistakes

--- zphisher.sh
+++ zphisher.sh
@@ -122,7 +122,7 @@ banner() {
 		${ORANGE}      | |                                
 		${ORANGE}      |_|                ${RED}Version : ${__version__}
 
-		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by htr-tech (tahmid.rayat)${WHITE}
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by pratheepraj (mr.360)${WHITE}
 	EOF
 }
 
